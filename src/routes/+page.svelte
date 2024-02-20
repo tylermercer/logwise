79b10e19b33ee32f13ad2b1474a19c1c8524f3e5
Tyler Mercer <acct@tylermercer.net>
2024-02-19 20:09:06 -0700

    Implement basic template adding flow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Link from '$lib/components/Link.svelte';
+</script>
+
+<h1>Log Thing</h1>
+<p><Link href="/templates">My templates</Link></p>
