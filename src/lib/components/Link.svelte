084f00ffafb6755f22b47f40ced0c46e51154f51
Tyler Mercer <acct@tylermercer.net>
2024-02-21 19:46:58 -0700

    Add class prop to Link

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { base } from '$app/paths';
 	export let href = '';
+	let className = '';
+    export { className as class };
 </script>
 
-<a href="{base}{href}"><slot /></a>
+<a href="{base}{href}" class={className}><slot /></a>
