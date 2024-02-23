ca1d70074743ab5b43b88f05d3a3c6df67a3f4d2
Tyler Mercer <acct@tylermercer.net>
2024-02-22 23:04:35 -0700

    Add service worker via @vite-pwa/sveltekit
    Does not precache files correctly yet. Need to figure that part out

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
+	import { pwaInfo } from 'virtual:pwa-info'; 
 	import MungedEmailLink from '$lib/components/MungedEmailLink.svelte';
 	import '../styles/base.scss';
 	import '../styles/layouts.scss';
 	import '../styles/utilities.scss';
+
+	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '' 
 </script>
+
+<svelte:head> 
+ 	{@html webManifestLink} 
+</svelte:head>
 
 <slot />
 <footer class="container">
