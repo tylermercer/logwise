509a4adcf060a41d459b1f1e073ee4144f2f0b52
Tyler Mercer <acct@tylermercer.net>
2024-02-22 19:49:40 -0700

    Add feedback email link to footer

---
+++ src/lib/components/MungedEmailLink.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	let className: string = '';
+	export { className as class };
+
+	function dec(str: string) {
+        return str.replace(/[a-zA-Z]/g, function (c: any) {
+            return String.fromCharCode(
+                (c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13)
+                    ? c
+                    : c - 26,
+            );
+        });
+    }
+
+	let mungedUser = 'ybtguvat.srrqonpx'; //logthing.feedback
+	let mungedDomain = 'glyrezrepre.arg'; //tylermercer.net
+</script>
+
+<a
+	href="mailto:{dec(`${mungedUser}@${mungedDomain}`)}"
+	class="{className}"
+	target="_blank"
+>
+	<slot></slot>
+</a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+	import MungedEmailLink from '$lib/components/MungedEmailLink.svelte';
 	import '../styles/base.scss';
 	import '../styles/layouts.scss';
 	import '../styles/utilities.scss';
 </script>
 
-<slot></slot>
-<footer class="container">Created by <a href="https://tylermercer.net">Tyler Mercer</a></footer>
+<slot />
+<footer class="container">
+	<small>
+		Created by <a class="secondary" href="https://tylermercer.net">Tyler Mercer</a>. Got feedback? <MungedEmailLink class="secondary">Email me!</MungedEmailLink>
+	</small>
+</footer>
