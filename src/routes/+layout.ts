59aeb699ff8c3782b4a8fa97fc6edd993fe3284c
Tyler Mercer <acct@tylermercer.net>
2024-04-27 19:27:43 -0600

    Log commit hash in browser console

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,6 @@
 import { browser } from '$app/environment';
 import db from '$lib/db';
+import logCommitHash from '$lib/util/logCommitHash';
 import type { LayoutLoad } from './$types';
 
 export const ssr = false;
@@ -7,6 +8,7 @@ export const ssr = false;
 export const load: LayoutLoad = async () => {
     if (browser) {
         await db.open();
+        logCommitHash();
     }
     return {
 		db,
