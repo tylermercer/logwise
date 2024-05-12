89cbef6daddd820686b4d526d3f7f23f7729e308
Tyler Mercer <acct@tylermercer.net>
2024-05-11 20:39:07 -0600

    Extract AnsweredQuestion types

--- src/lib/question/index.ts
+++ src/lib/question/index.ts
@@ -1,6 +1,7 @@
 import { getType, type TypeID } from 'typeid-unboxed';
-import type { TextQuestion } from './text';
-import type { LikertQuestion } from './likert';
+import { isTextQuestion, type TextQuestion, type AnsweredTextQuestion } from './text';
+import { isLikertQuestion, type LikertQuestion, type AnsweredLikertQuestion } from './likert';
+import assertNever from '$lib/util/assertNever';
 
 export interface BaseQuestion<T extends string> {
     text: string;
@@ -13,8 +14,35 @@ export type Question =
   | LikertQuestion
   ;
 
+export type AnsweredQuestion =
+AnsweredTextQuestion
+  | AnsweredLikertQuestion
+  ;
+
 export	type DraftQuestion = { tempId: string; type: 'text' | 'likert'; data: Omit<Question, 'id'> };
 
 export function makeTypeGuard<QType extends Question>(idType: string) {
     return (q: Question): q is QType => getType(q.id) == idType;
+}
+
+export function makeAnsweredTypeGuard<QType extends AnsweredQuestion>(idType: string) {
+  return (q: AnsweredQuestion): q is QType => getType(q.question.id) == idType;
+}
+
+export function toAnsweredQuestion(question: Question): AnsweredQuestion {
+  if (isTextQuestion(question)) {
+    return {
+      question,
+      answer: '',
+    }
+  }
+  else if (isLikertQuestion(question)) {
+    return {
+      question,
+      answer: 3
+    }
+  }
+  else {
+    return assertNever(question);
+  }
 }
