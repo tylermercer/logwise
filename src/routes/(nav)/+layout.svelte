88c95d67ee39656fb3822e7fb820272a70d3d0e6
Tyler Mercer <acct@tylermercer.net>
2024-06-26 21:53:21 -0600

    Group all pages into (nav) bucket

---
+++ src/routes/(nav)/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import MungedEmailLink from '$lib/components/MungedEmailLink.svelte';
+	import db from "$lib/db";
+	import LogIn from '$lib/components/LogIn.svelte';
+
+	let user = db.cloud.currentUser;
+</script>
+
+<slot />
+<footer class="u-guttered l-column l-space-xs">
+	<hr>
+	<div class="l-column l-space-xs">
+		{#if $user.isLoggedIn}
+			<div>
+				Logged in as {$user.name}. <a href="/app/" on:click={() => db.cloud.logout()}>Log out</a>
+			</div>
+			<div>License: {$user.license?.type ?? 'No license'} ({$user.license?.status ?? 'no status'})</div>
+		{:else}
+			<button on:click={() => db.cloud.login()}>Log in</button>
+		{/if}
+	</div>
+	<small>
+		Created by <a class="btn-secondary" href="https://tylermercer.net">Tyler Mercer</a>. Got feedback? <MungedEmailLink class="btn-secondary">Email me!</MungedEmailLink>
+	</small>
+</footer>
