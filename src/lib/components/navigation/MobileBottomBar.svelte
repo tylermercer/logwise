6ab1507438b2f733e3975c7dfd11f66a61e02baf
Tyler Mercer <acct@tylermercer.net>
2024-06-27 16:47:57 -0600

    Extract bottom bar into component

---
+++ src/lib/components/navigation/MobileBottomBar.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import mediaQueries from '$lib/config/mediaQueries';
+	import MediaQuery from 'svelte-media-queries';
+	import HomeIcon from 'virtual:icons/teenyicons/home-outline';
+	import LogIcon from 'virtual:icons/teenyicons/list-layout-outline';
+	import MenuIcon from 'virtual:icons/teenyicons/menu-outline';
+
+</script>
+
+<MediaQuery query={mediaQueries.mobile} let:matches>
+	{#if matches}
+		<nav class="bottom-bar l-row">
+			<a role="button" href="/app/">
+				<HomeIcon></HomeIcon>
+			</a>
+			<a role="button" href="/app/log">
+				<LogIcon></LogIcon>
+			</a>
+			<a role="button" href="/app/logs">
+				<MenuIcon></MenuIcon>
+			</a>
+		</nav>
+	{/if}
+</MediaQuery>
+
+<style lang="scss">
+	nav.bottom-bar {
+		position: fixed;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		align-items: stretch;
+		border-top: 1px solid var(--primary-6);
+		background-color: var(--primary-2);
+		& > a {
+			flex: 1;
+			display: flex;
+			align-items: center;
+			height: var(--bottom-bar-height);
+			justify-content: center;
+			color: var(--primary-9);
+			background-color: transparent;
+			&:active {
+				background-color: var(--primary-3);
+			}
+		}
+	}
+</style>
