19490a5423994e3401e2581663f1a7a52906dce0
Tyler Mercer <acct@tylermercer.net>
2024-06-27 16:47:57 -0600

    Extract bottom bar into component

--- src/routes/(nav)/+layout.svelte
+++ src/routes/(nav)/+layout.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
 	import MungedEmailLink from '$lib/components/MungedEmailLink.svelte';
-	import mediaQueries from '$lib/config/mediaQueries';
+	import MobileBottomBar from '$lib/components/navigation/MobileBottomBar.svelte';
 	import db from '$lib/db';
-	import MediaQuery from 'svelte-media-queries';
-	import HomeIcon from 'virtual:icons/teenyicons/home-outline';
-	import LogIcon from 'virtual:icons/teenyicons/list-layout-outline';
-	import MenuIcon from 'virtual:icons/teenyicons/menu-outline';
 
 	let user = db.cloud.currentUser;
 </script>
@@ -30,45 +26,10 @@
 		feedback? <MungedEmailLink class="btn-secondary">Email me!</MungedEmailLink>
 	</small>
 </footer>
-<MediaQuery query={mediaQueries.mobile} let:matches>
-	{#if matches}
-		<nav class="bottom-bar l-row">
-			<a role="button" href="/app/">
-				<HomeIcon></HomeIcon>
-			</a>
-			<a role="button" href="/app/log">
-				<LogIcon></LogIcon>
-			</a>
-			<a role="button" href="/app/logs">
-				<MenuIcon></MenuIcon>
-			</a>
-		</nav>
-	{/if}
-</MediaQuery>
+<MobileBottomBar/>
 
 <style lang="scss">
 	footer {
-		margin-bottom: 44px;
-	}
-	nav.bottom-bar {
-		position: fixed;
-		left: 0;
-		bottom: 0;
-		width: 100%;
-		align-items: stretch;
-		border-top: 1px solid var(--primary-6);
-		background-color: var(--primary-2);
-		& > a {
-			flex: 1;
-			display: flex;
-			align-items: center;
-			height: 44px;
-			justify-content: center;
-			color: var(--primary-9);
-			background-color: transparent;
-			&:active {
-				background-color: var(--primary-3);
-			}
-		}
+		margin-bottom: var(--bottom-bar-height);
 	}
 </style>
