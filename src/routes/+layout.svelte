73608abb38b3b8ba294b7797c21124b2132ad047
Tyler Mercer <acct@tylermercer.net>
2024-06-26 21:53:21 -0600

    Group all pages into (nav) bucket

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,36 +1,16 @@
 <script lang="ts">
-	import MungedEmailLink from '$lib/components/MungedEmailLink.svelte';
+	import LogIn from '$lib/components/LogIn.svelte';
 	import '@fontsource-variable/figtree';
 	import '@fontsource-variable/rubik';
 	import "modern-normalize/modern-normalize.css";
-	import '../styles/theme.scss';
 	import '../styles/base.scss';
 	import '../styles/layouts.scss';
+	import '../styles/theme.scss';
 	import '../styles/utilities.scss';
-	import db from "$lib/db";
-	import LogIn from '$lib/components/LogIn.svelte';
-
-	let user = db.cloud.currentUser;
 </script>
 
 <svelte:head>
 	<title>Logwise</title>
 </svelte:head>
 <slot />
-<footer class="u-guttered l-column l-space-xs">
-	<hr>
-	<div class="l-column l-space-xs">
-		{#if $user.isLoggedIn}
-			<div>
-				Logged in as {$user.name}. <a href="/app/" on:click={() => db.cloud.logout()}>Log out</a>
-			</div>
-			<div>License: {$user.license?.type ?? 'No license'} ({$user.license?.status ?? 'no status'})</div>
-		{:else}
-			<button on:click={() => db.cloud.login()}>Log in</button>
-		{/if}
-	</div>
-	<small>
-		Created by <a class="btn-secondary" href="https://tylermercer.net">Tyler Mercer</a>. Got feedback? <MungedEmailLink class="btn-secondary">Email me!</MungedEmailLink>
-	</small>
-</footer>
 <LogIn/>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,4 @@
 import { browser } from '$app/environment';
-import db from '$lib/db';
 import logCommitHash from '$lib/util/logCommitHash';
 import type { LayoutLoad } from './$types';
 
