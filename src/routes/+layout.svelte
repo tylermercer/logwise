46fc13c51c8584b1af0c3ccb585e3ee7287e1981
Tyler Mercer <acct@tylermercer.net>
2024-06-27 17:17:28 -0600

    Add sidebar with NavMenu in Desktop layouts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,37 @@
 <script lang="ts">
-	import LogIn from '$lib/components/LogIn.svelte';
 	import '../styles/styles.scss';
+	import LogIn from '$lib/components/LogIn.svelte';
+	import NavMenu from '$lib/components/navigation/NavMenu.svelte';
+	import MediaQuery from 'svelte-media-queries';
+	import mediaQueries from '$lib/config/mediaQueries';
 </script>
 
 <svelte:head>
 	<title>Logwise</title>
 </svelte:head>
-<slot />
-<LogIn/>
+<div class="with-maybe-sidebar">
+	<MediaQuery query={mediaQueries.mobile} let:matches>
+		{#if !matches}
+			<nav class="sidebar">
+				<NavMenu></NavMenu>
+			</nav>
+		{/if}
+	</MediaQuery>
+	<div>
+		<slot />
+	</div>
+</div>
+<LogIn />
+
+<style lang="scss">
+	.with-maybe-sidebar {
+		display: flex;
+		align-items: stretch;
+		min-height: 100vh;
+		min-height: 100lvh;
+	}
+	.sidebar {
+		width: 20rem;
+		background-color: var(--primary-3);
+	}
+</style>
