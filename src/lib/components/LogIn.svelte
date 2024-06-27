7349048cee6da09db77e545b6949cb9fb8c7e92c
Tyler Mercer <acct@tylermercer.net>
2024-06-26 21:23:22 -0600

    Improve appearance of Login modal

--- src/lib/components/LogIn.svelte
+++ src/lib/components/LogIn.svelte
@@ -2,7 +2,7 @@
 	import db from '$lib/db';
 	import { resolveText, type DXCUserInteraction } from 'dexie-cloud-addon';
 
-    type Params = { [param: string]: string };
+	type Params = { [param: string]: string };
 
 	let params = {} as Params;
 	let ui = db.cloud.userInteraction;
@@ -18,14 +18,17 @@
 		};
 	});
 
-    function handleSubmit(e: Event, ui: DXCUserInteraction) {
-        e.preventDefault();
-        ui.onSubmit(params);
-    }
+	function handleSubmit(e: Event, ui: DXCUserInteraction) {
+		e.preventDefault();
+		ui.onSubmit(params);
+	}
+	function showModal(e: HTMLDialogElement) {
+		e.showModal();
+	}
 </script>
 
 {#if $ui}
-	<dialog open>
+	<dialog use:showModal class="l-column l-space-s">
 		<h2>Log in to Log Thing</h2>
 		<p>{$ui.title}</p>
 		{#each $ui.alerts as alert, i}
@@ -33,26 +36,26 @@
 		{/each}
 		<form on:submit={(e) => handleSubmit(e, $ui)} class="l-column">
 			{#each entries as e}
-				<label class="l-column">
+				<label>
 					{e.label ? `${e.label}: ` : e.fieldName}
 					<input
-                        class="dlg-input"
+						class="dlg-input"
 						type={e.type}
 						name={e.fieldName}
 						placeholder={e.placeholder}
-						value={(params[e.fieldName] ?? '')}
+						value={params[e.fieldName] ?? ''}
 						on:input={(ev) => updateField(ev, e.fieldName)}
 					/>
 				</label>
 			{/each}
-            <div class="l-cluster-l">
-                <button type="submit">
-                    {$ui.submitLabel}
-                </button>
-                {#if $ui.cancelLabel}
-                    <button on:click={$ui.onCancel}>{$ui.cancelLabel}</button>
-                {/if}
-            </div>
+			<div class="l-cluster-r l-space-xs">
+				{#if $ui.cancelLabel}
+					<button class="btn-secondary" on:click={$ui.onCancel}>{$ui.cancelLabel}</button>
+				{/if}
+				<button type="submit">
+					{$ui.submitLabel}
+				</button>
+			</div>
 		</form>
 	</dialog>
 {/if}
@@ -61,7 +64,6 @@
 	dialog::backdrop {
 		opacity: 0.5;
 		background-color: #000;
-		z-index: 150;
 		backdrop-filter: blur(2px);
 		-webkit-backdrop-filter: blur(2px);
 	}
@@ -70,24 +72,14 @@
 		top: 20%;
 		color: #222;
 		background-color: #fff;
-		padding: 30px;
-		margin-bottom: 2em;
+		padding: var(--space-s);
 		max-width: 90%;
 		max-height: 60%;
 		overflow-y: auto;
-		border: 3px solid #3d3d5d;
-		border-radius: 8px;
-		box-shadow: 0 0 80px 10px #666;
+		border: 3px solid var(--space-s);
+		border-radius: var(--space-xs);
 		width: auto;
 		font-family: sans-serif;
-	}
-	.dlg-input {
-		height: 35px;
-		width: 17em;
-		border-color: #ccf4;
-		outline: none;
-		font-size: 17pt;
-		padding: 8px;
 	}
 	.dxcdlg-alert-error {
 		color: red;
