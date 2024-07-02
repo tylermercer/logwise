820790629b3e427d0ffedc763ced5ce00d4cd11e
Tyler Mercer <acct@tylermercer.net>
2024-07-02 08:14:47 -0600

    Add UserAccountMenu using melt dropdown
    Not perfect but better

---
+++ src/lib/components/account/UserAccountMenu.svelte
@@ -0,0 +1,113 @@
+<script lang="ts">
+	import { createDropdownMenu, melt } from '@melt-ui/svelte';
+	import db from '$lib/db';
+	import { fly } from 'svelte/transition';
+
+	const {
+		elements: { trigger, menu, item, separator, arrow },
+		states: { open }
+	} = createDropdownMenu({
+		forceVisible: true,
+		loop: true
+	});
+
+	let user = db.cloud.currentUser;
+</script>
+
+<div class="menu-container l-row l-space-xs">
+    {#if $user.isLoggedIn}
+        <button type="button" class="trigger" use:melt={$trigger} aria-label="Open Popover"> ac </button>
+    
+        {#if $open}
+            <div class="menu l-column l-space-2xs" use:melt={$menu} transition:fly={{ duration: 150, y: -10 }}>
+                <div class="item info">Signed in as {$user.name}</div>
+                {#if $user.license?.type !== 'prod'}
+                <div class="upgrade-container">
+                    <button class="btn-upgrade">Upgrade</button>
+                </div>
+                {/if}
+                <div class="separator" use:melt={$separator} />
+                <div class="item" use:melt={$item} on:m-click={() => db.cloud.logout()}>Sign out</div>
+                <div use:melt={$arrow} />
+            </div>
+        {/if}
+    {:else}
+        <button on:click={() => db.cloud.login()}>Sign in</button>
+    {/if}
+</div>
+
+<style lang="scss">
+	.menu {
+		z-index: 40;
+		display: flex;
+		max-height: 300px;
+		min-width: 220px;
+		background-color: var(--gray-1);
+		color: var(--gray-12);
+		flex-direction: column;
+		border-radius: var(--space-2xs);
+		padding: var(--space-xs);
+		box-shadow:
+			0 10px 15px -3px rgba(0, 0, 0, 0.1),
+			0 4px 6px -2px rgba(0, 0, 0, 0.05);
+	}
+    .btn-upgrade {
+        display: inline-block;
+        font-size: var(--step--1);
+        padding: 0.5rem;
+    }
+    .upgrade-container {
+        padding: 0.25rem;
+    }
+	.item {
+		position: relative;
+		height: 24px;
+		min-height: 24px;
+		user-select: none;
+		border-radius: 0.125rem;
+		padding-left: 0.25rem;
+		padding-right: 0.25rem;
+		z-index: 40;
+		color: var(--gray-12);
+		display: flex;
+		align-items: center;
+		font-size: 0.875rem;
+		line-height: 1;
+		&:focus {
+			outline: none;
+		}
+		&[data-highlighted] {
+			background-color: var(--gray-3);
+			color: var(--gray-12);
+			outline: none;
+		}
+		&[data-disabled] {
+			color: var(--gray-5);
+		}
+		&.info {
+			color: var(--gray-11);
+		}
+	}
+	.trigger {
+		display: inline-flex;
+		height: 2.25rem;
+		width: 2.25rem;
+		align-items: center;
+		justify-content: center;
+		border-radius: 9999px;
+		background-color: var(--primary-9);
+		color: var(--gray-1);
+		&[data-highlighted] {
+			outline: 2px solid var(--primary-6);
+			outline-offset: 2px;
+		}
+		padding: 0;
+		font-size: 0.875rem;
+		font-weight: 500;
+	}
+	.separator {
+		margin: 0.3125rem 0;
+		height: 1px;
+		background-color: #e2e8f0;
+	}
+</style>
