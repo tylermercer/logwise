0a44052770f9a027bb8ad4f2aa8128a39f39222b
Tyler Mercer <acct@tylermercer.net>
2024-07-02 22:57:50 -0600

    Wire up feedback and settings buttons

--- src/lib/components/MungedEmailLink.svelte
+++ src/lib/components/MungedEmailLink.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	let className: string = '';
 	export { className as class };
+	export let role: string | undefined = undefined;
+	export let ariaLabel: string | undefined = undefined;
 
 	function dec(str: string) {
         return str.replace(/[a-zA-Z]/g, function (c: any) {
@@ -20,6 +22,8 @@
 	href="mailto:{dec(`${mungedUser}@${mungedDomain}`)}"
 	class="{className}"
 	target="_blank"
+	role={role}
+	aria-label={ariaLabel}
 >
 	<slot></slot>
 </a>
--- src/lib/components/navigation/NavMenu.svelte
+++ src/lib/components/navigation/NavMenu.svelte
@@ -1,50 +1,29 @@
 <script lang="ts">
-	import Message from 'virtual:icons/teenyicons/message-text-alt-outline';
-	import Cog from 'virtual:icons/teenyicons/cog-outline';
 	import MungedEmailLink from '$lib/components/MungedEmailLink.svelte';
+	import Cog from 'virtual:icons/teenyicons/cog-outline';
+	import Message from 'virtual:icons/teenyicons/message-text-alt-outline';
 	import LogsList from "../LogsList.svelte";
-	import db from '$lib/db';
 	import UserAccountMenu from '../account/UserAccountMenu.svelte';
-
-	let user = db.cloud.currentUser;
 </script>
-
 
 <header class="header u-guttered l-column l-space-xs">
 	<div class="l-column l-space-xs">
 		<UserAccountMenu>
 			<div class="buttons l-row l-space-none">
-				<button class="btn-icon" aria-label="Give feedback">
+				<MungedEmailLink role="button" class="btn-icon" ariaLabel="Give feedback">
 					<Message></Message>
-				</button>
-				<button class="btn-icon" aria-label="Settings">
+				</MungedEmailLink>
+				<a href="/app/settings" role="button" class="btn-icon" aria-label="Settings">
 					<Cog></Cog>
-				</button>
+				</a>
 			</div>
 		</UserAccountMenu>
 	</div>
 </header>
 <slot></slot>
 <LogsList></LogsList>
-<footer class="u-guttered">
-	<small>
-		Created by <a class="btn-secondary" href="https://tylermercer.net">Tyler Mercer</a>. Got
-		feedback? <MungedEmailLink class="btn-secondary">Email me!</MungedEmailLink>
-	</small>
-</footer>
+
 <style lang="scss">
-	.btn-icon {
-		color: var(--primary-11);
-		background-color: transparent;
-		aspect-ratio: 1;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		&:hover {
-			background-color: var(--primary-4);
-			color: var(--primary-12);
-		}
-	}
 	.buttons {
 		margin-right: calc(var(--space-xs) * -1);
 	}
