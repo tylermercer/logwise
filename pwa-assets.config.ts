0b2e4eae0e6163e0d9bfc1d3fee2ba111a082236
Tyler Mercer <acct@tylermercer.net>
2024-07-03 21:07:49 -0600

    Configure maskable icon background color

---
+++ pwa-assets.config.ts
@@ -0,0 +1,23 @@
+import {
+    defineConfig,
+    minimal2023Preset as preset
+  } from '@vite-pwa/assets-generator/config'
+  
+console.log(preset);
+
+  export default defineConfig({
+    headLinkOptions: {
+      preset: '2023'
+    },
+    preset: {
+        ...preset,
+        maskable: {
+            ...preset.maskable,
+            resizeOptions: {
+                ...preset.maskable.resizeOptions,
+                background: '#001347'
+            }
+        }
+    },
+    images: ['public/logo.svg']
+  })
