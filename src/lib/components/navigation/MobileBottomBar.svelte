2aebbc71b82bcd94a30ede2797a370f50153993b
Tyler Mercer <acct@tylermercer.net>
2024-07-04 22:05:10 -0600

    Update aria-current in nav via Svelte action
    Also added labels to nav links

--- src/lib/components/navigation/MobileBottomBar.svelte
+++ src/lib/components/navigation/MobileBottomBar.svelte
@@ -7,6 +7,7 @@
 	import MenuIcon from 'virtual:icons/teenyicons/menu-outline';
 	import CloseIcon from 'virtual:icons/teenyicons/x-outline';
 	import { page } from '$app/stores';
+	import ariaCurrent from '$lib/util/actions/ariaCurrent';
 
 	const dispatch = createEventDispatcher();
 
@@ -18,18 +19,21 @@
 <MediaQuery query={mediaQueries.mobile} let:matches>
 	{#if matches}
 		<nav class="u-mobile-only bottom-bar l-row">
-			<a role="button" href="/app/">
+			<a class="bar-link" use:ariaCurrent role="button" href="/app/">
 				<HomeIcon></HomeIcon>
+				<span class="bar-link-label">Home</span>
 			</a>
-			<a role="button" href="/app/log">
+			<a class="bar-link" use:ariaCurrent role="button" href="/app/log">
 				<LogIcon></LogIcon>
+				<span class="bar-link-label">Unified Log</span>
 			</a>
-			<button on:click={toggleMenu}>
+			<button class="bar-link" on:click={toggleMenu}>
 				{#if $page.state.showMenu}
 					<CloseIcon></CloseIcon>
 				{:else}
 					<MenuIcon></MenuIcon>
 				{/if}
+				<span class="bar-link-label">Menu</span>
 			</button>
 		</nav>
 	{/if}
@@ -44,17 +48,21 @@
 		align-items: stretch;
 		border-top: 1px solid var(--primary-6);
 		background-color: var(--primary-2);
-		& > a,
-		& > button {
+		& > .bar-link {
 			flex: 1;
 			display: flex;
 			align-items: center;
+			flex-direction: column;
+			gap: var(--space-3xs);
 			height: var(--bottom-bar-height);
 			justify-content: center;
 			color: var(--primary-9);
 			background-color: transparent;
 			&:active {
 				background-color: var(--primary-3);
+			}
+			& > .bar-link-label {
+				font-size: var(--step--1);
 			}
 		}
 	}
