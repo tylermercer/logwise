71853cc3f1f83f81875e5dd416c488ad41b2155a
Tyler Mercer <acct@tylermercer.net>
2024-07-06 12:36:38 -0600

    Add tooltips to settings and feedback buttons

--- src/lib/components/navigation/NavMenu.svelte
+++ src/lib/components/navigation/NavMenu.svelte
@@ -4,18 +4,23 @@
 	import Message from 'virtual:icons/teenyicons/message-text-alt-outline';
 	import LogsList from "../LogsList.svelte";
 	import UserAccountMenu from '../account/UserAccountMenu.svelte';
+	import Tooltip from '../Tooltip.svelte';
 </script>
 
 <header class="header u-guttered l-column l-space-xs">
 	<div class="l-column l-space-xs">
 		<UserAccountMenu>
 			<div class="buttons l-row l-space-none">
-				<MungedEmailLink role="button" class="btn-icon" ariaLabel="Give feedback">
-					<Message></Message>
-				</MungedEmailLink>
-				<a href="/app/settings" role="button" class="btn-icon" aria-label="Settings">
-					<Cog></Cog>
-				</a>
+				<Tooltip text="Give feedback">
+					<MungedEmailLink role="button" class="btn-icon" ariaLabel="Give feedback">
+						<Message></Message>
+					</MungedEmailLink>
+				</Tooltip>
+				<Tooltip text="Settings">
+					<a href="/app/settings" role="button" class="btn-icon" aria-label="Settings">
+						<Cog></Cog>
+					</a>
+				</Tooltip>
 			</div>
 		</UserAccountMenu>
 	</div>
