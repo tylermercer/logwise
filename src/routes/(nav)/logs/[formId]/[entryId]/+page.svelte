4d6ebbcba0a4979a40dead6574c05f23328738f3
Tyler Mercer <acct@tylermercer.net>
2024-07-11 06:20:37 -0600

    Add edited and created datetimes to entry footer

--- src/routes/(nav)/logs/[formId]/[entryId]/+page.svelte
+++ src/routes/(nav)/logs/[formId]/[entryId]/+page.svelte
@@ -4,6 +4,7 @@
 	import LogEntry from '$lib/components/LogEntry.svelte';
 	import { goto } from '$app/navigation';
 	import db from '$lib/db';
+	import { dateToString } from '$lib/util/dateUtils';
 
 	export let data: PageData;
 
@@ -28,7 +29,11 @@
 </svelte:head>
 <HeaderBar backHref="/app/logs">
 	<h1>Entry</h1>
-	<a class="btn-edit btn-secondary btn-outline" role="button" href={`/app/logs/${data.existingEntry.formId}/${data.existingEntry.id}/edit`}>
+	<a
+		class="btn-edit btn-secondary btn-outline"
+		role="button"
+		href={`/app/logs/${data.existingEntry.formId}/${data.existingEntry.id}/edit`}
+	>
 		Edit
 	</a>
 	<button
@@ -44,8 +49,18 @@
 		{/if}
 	</button>
 </HeaderBar>
-<main class="u-guttered">
+<main class="u-guttered l-column l-space-s">
 	<LogEntry entry={existingEntry}></LogEntry>
+	<div class="footnotes l-column l-space-none">
+		<small>
+			Created {dateToString(data.existingEntry.createdDatetime)}
+		</small>
+		{#if data.existingEntry.createdDatetime !== data.existingEntry.modifiedDatetime}
+			<small>
+				Modified {dateToString(data.existingEntry.modifiedDatetime)}
+			</small>
+		{/if}
+	</div>
 </main>
 
 <style lang="scss">
