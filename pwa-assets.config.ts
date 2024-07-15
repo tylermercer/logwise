04dc45ef7e45a8405c0e7d88cc7cd2d65cbf3ff4
Tyler Mercer <acct@tylermercer.net>
2024-07-15 06:53:10 -0600

    Format PWA assets config

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -1,23 +1,21 @@
 import {
-    defineConfig,
-    minimal2023Preset as preset
-  } from '@vite-pwa/assets-generator/config'
-  
-console.log(preset);
+  defineConfig,
+  minimal2023Preset as preset
+} from '@vite-pwa/assets-generator/config'
 
-  export default defineConfig({
-    headLinkOptions: {
-      preset: '2023'
-    },
-    preset: {
-        ...preset,
-        maskable: {
-            ...preset.maskable,
-            resizeOptions: {
-                ...preset.maskable.resizeOptions,
-                background: '#001347'
-            }
-        }
-    },
-    images: ['public/logo.svg']
-  })
+export default defineConfig({
+  headLinkOptions: {
+    preset: '2023'
+  },
+  preset: {
+    ...preset,
+    maskable: {
+      ...preset.maskable,
+      resizeOptions: {
+        ...preset.maskable.resizeOptions,
+        background: '#001347'
+      }
+    }
+  },
+  images: ['public/logo.svg']
+})
