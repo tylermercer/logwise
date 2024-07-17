75e124a15ebb737b96854a6d53f32888a05ff453
Tyler Mercer <acct@tylermercer.net>
2024-07-17 07:00:23 -0600

    Move DB module into folder
    For tidiness

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,7 +2,7 @@ import Dexie, { type Table } from 'dexie';
 import dexieCloud from "dexie-cloud-addon";
 import { type TypeID } from 'typeid-unboxed';
 import { PUBLIC_DEXIE_CLOUD_URL } from '$env/static/public';
-import type { Question } from './question';
+import type { Question } from '../question';
 
 export type QuestionId = Question['id']
 
