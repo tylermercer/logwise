fd0c26a53109d753b5c74dab2a770f6d957aaee0
Tyler Mercer <acct@tylermercer.net>
2024-07-17 07:13:52 -0600

    Add basic test for Dexie data loading and reading

---
+++ src/lib/db/db.test.ts
@@ -0,0 +1,38 @@
+import { expect, test } from 'vitest'
+import "fake-indexeddb/auto"; // must import before DB module
+import db, { DB_NULL } from '.';
+import { typeid } from 'typeid-unboxed';
+
+test('loading data works', async () => {
+  db.forms.bulkAdd([
+    {
+      id: typeid('form'),
+      name: 'Test form 1',
+      modifiedDatetime: new Date(),
+      createdDatetime: new Date(),
+      prevVersionId: DB_NULL,
+      nextVersionId: DB_NULL,
+      questions: []
+    },
+    {
+      id: typeid('form'),
+      name: 'Test form 2',
+      modifiedDatetime: new Date(),
+      createdDatetime: new Date(),
+      prevVersionId: DB_NULL,
+      nextVersionId: DB_NULL,
+      questions: []
+    },
+    {
+      id: typeid('form'),
+      name: 'Test form 3',
+      modifiedDatetime: new Date(),
+      createdDatetime: new Date(),
+      prevVersionId: DB_NULL,
+      nextVersionId: DB_NULL,
+      questions: []
+    },
+  ]);
+  const count = await db.forms.count();
+  expect(count).toBe(3);
+})
