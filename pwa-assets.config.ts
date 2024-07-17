47331e76d771b4e745fd05bd8a6b192c9adadabe
Tyler Mercer <acct@tylermercer.net>
2024-07-17 06:33:05 -0600

    Convert updatePwaAssetColor script into module
    To deduplicate the getAccentColor (now getPrimaryColor) function.
    
    Having the config module have a side effect is a bit weird, but it
    works for our purposes and is much better than dealing with cjs.

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -2,19 +2,14 @@ import {
   defineConfig,
   minimal2023Preset as preset
 } from '@vite-pwa/assets-generator/config'
-import fs from 'fs';
+import {
+  getThemeColor,
+  interpolateThemeColorIntoAssets,
+} from './src/plugins/pwa-assets/update-pwa-asset-color';
 
-function getAccentColor() {
-  const scssContent = fs.readFileSync('./src/styles/theme.scss', 'utf8');
-  const colorMatch = scssContent.match(/\$accentColor:\s*'([^']+)';/);
-  if (colorMatch && colorMatch[1]) {
-    return colorMatch[1];
-  } else {
-    throw new Error('Accent color not found in theme file');
-  }
-}
+const color = getThemeColor();
 
-const color = getAccentColor();
+interpolateThemeColorIntoAssets();
 
 export default defineConfig({
   headLinkOptions: {
