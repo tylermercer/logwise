6ab67269e4893e833fc6e5addda8c702a44fe366
Tyler Mercer <acct@tylermercer.net>
2024-07-31 19:52:10 -0600

    Make db accessible from window in dev mode

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,3 +1,4 @@
+import { browser, dev } from "$app/environment";
 import AppDexie, {
   DB_CURRENT_ENTITY_VERSION,
   DB_FALSE,
@@ -33,12 +34,20 @@ export {
   type VersionedSchemaEntity,
 }
 
-const db = new AppDexie();
+const db = new AppDexie(dev);
 
 //Note that this invokes db.open() when the module is loaded
 export const dbOpenPromise = new Promise<void>(r => db.open()
   .then(() => r()))
   .then(runMigrationsIfNeeded)
   .then(() => db.cloud.events.syncComplete.subscribe(() => runMigrationsIfNeeded()));
+
+if (dev && browser) {
+  const devWindow = window as any;
+  devWindow.db = db;
+  devWindow.runMigrationsIfNeeded = runMigrationsIfNeeded;
+  devWindow.pull = () => db.cloud.sync({purpose: 'pull', wait: true});
+  devWindow.push = () => db.cloud.sync({purpose: 'push', wait: true});
+}
 
 export default db;
