5b819a5908e68fa2e2ab7d12f84e504cc56660c5
Tyler Mercer <acct@tylermercer.net>
2024-08-09 07:52:32 -0600

    Remove duplicated log name on per-log view
    Uses first question for logs with questions, and
    uses time for logs with no questions.

--- src/routes/(nav)/logs/+page.svelte
+++ src/routes/(nav)/logs/+page.svelte
@@ -13,6 +13,6 @@
 </HeaderBar>
 <EntriesList paginatedQuery={getAllEntriesPaginated}>
 	<a slot="entry" let:entry href="/app/logs/{entry.log.id}/{entry.id}" class="u-link-block">
-		<LogEntry {entry}></LogEntry>
+		<LogEntry {entry} showLogName></LogEntry>
 	</a>
 </EntriesList>
