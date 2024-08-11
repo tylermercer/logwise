8d4f9a925cb533a26e5eda1523df90051a46cb8f
Tyler Mercer <acct@tylermercer.net>
2024-08-10 23:09:27 -0600

    Add entry duplicate action

--- src/routes/(nav)/logs/[logId]/[entryId]/+page.svelte
+++ src/routes/(nav)/logs/[logId]/[entryId]/+page.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import HeaderBar from '$lib/components/navigation/HeaderBar.svelte';
-	import LogEntry from '$lib/components/entries/LogEntry.svelte';
 	import { goto } from '$app/navigation';
-	import db from '$lib/db';
-	import { dateToString } from '$lib/util/dateUtils';
-	import PencilIcon from 'virtual:icons/teenyicons/edit-outline';
-	import TrashIcon from 'virtual:icons/teenyicons/bin-outline';
+	import LogEntry from '$lib/components/entries/LogEntry.svelte';
+	import HeaderBar from '$lib/components/navigation/HeaderBar.svelte';
 	import DropdownMenu from '$lib/components/util/dropdown-menu/DropdownMenu.svelte';
 	import DropdownMenuItem from '$lib/components/util/dropdown-menu/DropdownMenuItem.svelte';
 	import Tooltip from '$lib/components/util/Tooltip.svelte';
+	import db from '$lib/db';
 	import type { ExtendedEntry } from '$lib/db/types/ExtendedEntry';
+	import { dateToString } from '$lib/util/dateUtils';
+	import { typeid } from 'typeid-unboxed';
+	import TrashIcon from 'virtual:icons/teenyicons/bin-outline';
+	import CopyIcon from 'virtual:icons/teenyicons/documents-outline';
+	import PencilIcon from 'virtual:icons/teenyicons/edit-outline';
+	import type { PageData } from './$types';
 
 	export let data: PageData;
 
@@ -20,9 +22,21 @@
 		await db.entries.delete(data.entry.id);
 		goto('/app/logs', { replaceState: true });
 	}
-	
-	let extendedEntry : ExtendedEntry;
-	$: extendedEntry = { ...data.entry, form: data.form, log: data.log }
+
+	async function duplicateEntry() {
+		const newEntryId = typeid('entry');
+		const now = new Date();
+		await db.entries.add({
+			...data.entry,
+			id: newEntryId,
+			createdDatetime: now,
+			modifiedDatetime: now
+		});
+		goto(`/app/logs/${data.log.id}/${newEntryId}`);
+	}
+
+	let extendedEntry: ExtendedEntry;
+	$: extendedEntry = { ...data.entry, form: data.form, log: data.log };
 </script>
 
 <svelte:head>
@@ -41,9 +55,14 @@
 			</a>
 		</Tooltip>
 		<DropdownMenu class="u-icon-button-group-right">
+			<DropdownMenuItem on:item-click={duplicateEntry}>
+				<CopyIcon />
+				<span>Duplicate</span>
+			</DropdownMenuItem>
+			<hr />
 			<DropdownMenuItem class="u-danger" on:item-click={deleteEntry}>
-					<TrashIcon />
-					<span>Delete</span>
+				<TrashIcon />
+				<span>Delete</span>
 			</DropdownMenuItem>
 		</DropdownMenu>
 	</svelte:fragment>
