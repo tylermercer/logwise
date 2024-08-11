f4e620eaae6b855533d59475f1d50f35587d090f
Tyler Mercer <acct@tylermercer.net>
2024-08-10 18:29:05 -0600

    Improve alignment in header bar

--- src/lib/components/navigation/HeaderBar.svelte
+++ src/lib/components/navigation/HeaderBar.svelte
@@ -2,26 +2,30 @@
 	import backButtonLink from '$lib/util/actions/backButtonLink';
 	import LeftArrow from 'virtual:icons/teenyicons/left-outline';
 	export let backHref: string = '';
-
-
 </script>
 
 <header class="u-guttered l-row l-space-xs">
 	{#if backHref}
-	<a href={backHref} use:backButtonLink class="btn-secondary" aria-label="Home">
-		<LeftArrow></LeftArrow>
-	</a>
+		<a href={backHref} use:backButtonLink role="button" class="btn-icon" aria-label="Home">
+			<LeftArrow></LeftArrow>
+		</a>
 	{/if}
-	<slot></slot>
+	<slot />
 	<div class="buttons l-row l-space-none">
-		<slot name="actions"></slot>
+		<slot name="actions" />
 	</div>
 </header>
+
 <style lang="scss">
 	header {
 		flex-wrap: nowrap;
-		align-items: center;
+		align-items: flex-start;
 		padding-block: var(--space-m);
+		
+		& > :global(h1) {
+			height: 100%;
+			padding-block: var(--space-3xs);
+		}
 	}
 	.buttons {
 		margin-left: auto;
