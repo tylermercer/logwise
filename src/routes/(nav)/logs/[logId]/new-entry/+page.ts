b2b3b2e7ac9771d6139708abc7c1e9d247d49100
Tyler Mercer <acct@tylermercer.net>
2024-08-12 08:00:07 -0600

    Disallow new entry route for archived logs

--- src/routes/(nav)/logs/[logId]/new-entry/+page.ts
+++ src/routes/(nav)/logs/[logId]/new-entry/+page.ts
@@ -2,11 +2,12 @@ import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 import db from '$lib/db';
 import { fromString } from 'typeid-unboxed';
+import { DB_TRUE } from '$lib/db/types';
 
 export const load: PageLoad = async ({ params }) => {
     const log = await db.logs.get(fromString(params.logId, 'log'));
     const form = log ? await db.forms.get(log?.currentFormId) : undefined;
-    if (log && form) {
+    if (log && form && log.isArchived !== DB_TRUE) {
         return {
             log,
             form,
