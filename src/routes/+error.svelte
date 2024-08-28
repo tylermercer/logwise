6828900130e79b30d9b1aab75415b8ae8366e02c
Tyler Mercer <acct@tylermercer.net>
2024-08-27 20:50:42 -0600

    Add improved error page
    Just renders the error message to the page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+</script>
+
+<svelte:head>
+	<title>{$page.error?.message ?? "An error occurred"}</title>
+</svelte:head>
+
+<main class="u-guttered">
+    <h1>{$page.error?.message ?? "An error occurred"}</h1>
+</main>
