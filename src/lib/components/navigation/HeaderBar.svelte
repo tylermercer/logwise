3a792fc1ea271ff9afccc23aeab0b77543e01fc9
Tyler Mercer <acct@tylermercer.net>
2024-09-26 07:03:05 -0600

    Shift back button in header bar left on desktop
    Uses custom media query to make it not overlap sidebar at medium widths

--- src/lib/components/navigation/HeaderBar.svelte
+++ src/lib/components/navigation/HeaderBar.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
 	import backButtonLink from '$lib/util/actions/backButtonLink';
+	import MediaQuery from 'svelte-media-queries';
 	import LeftArrow from 'virtual:icons/teenyicons/left-outline';
 	export let backHref: string = '';
 </script>
 
 <header class="u-guttered l-row l-space-xs">
 	{#if backHref}
-		<a href={backHref} use:backButtonLink role="button" class="btn-icon" aria-label="Home">
-			<LeftArrow></LeftArrow>
-		</a>
+		<MediaQuery query="(max-width: 933px)" let:matches>
+			<a
+				href={backHref}
+				use:backButtonLink
+				role="button"
+				class="btn-icon"
+				class:shift-left={!matches}
+				aria-label="Home"
+			>
+				<LeftArrow></LeftArrow>
+			</a>
+		</MediaQuery>
 	{/if}
 	<slot />
 	<div class="buttons l-row l-space-none">
@@ -21,7 +31,7 @@
 		flex-wrap: nowrap;
 		align-items: flex-start;
 		padding-block: var(--space-m);
-		
+
 		& > :global(h1) {
 			flex-basis: 0;
 			min-width: 0;
@@ -29,6 +39,10 @@
 			height: 100%;
 			padding-block: var(--space-3xs);
 		}
+	}
+	.shift-left {
+		position: absolute;
+		transform: translateX(calc(-100% - var(--l-space)));
 	}
 	.buttons {
 		margin-left: auto;
