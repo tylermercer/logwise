705738a3fe03346987f9e53c9a5268a820af6157
Tyler Mercer <acct@tylermercer.net>
2024-09-26 07:42:52 -0600

    Add AddWidgetButton and blank new-widget page

---
+++ src/routes/(nav)/new-widget/+page@.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Add Widget</title>
+</svelte:head>
+<header class="u-guttered">
+	<h1>Add Widget</h1>
+</header>
+<main class="u-guttered l-prose">
+</main>
